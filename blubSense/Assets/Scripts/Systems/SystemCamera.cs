00a67a728f9a8c7bb6d6d63c91c0718a65432cb6
Wenish <jonas.voland@bluewin.ch>
2018-06-22 18:35:41 +0200

    update

---
+++ blubSense/Assets/Scripts/Systems/SystemCamera.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Scripts.Components;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Assets.Scripts.Systems
+{
+    public class SystemCamera : ComponentSystem
+    {
+        private struct GroupCamera
+        {
+            public Camera Camera;
+        }
+        private struct GroupPlayer
+        {
+            Player Player;
+        }
+        protected override void OnUpdate()
+        {
+            foreach (var entity in GetEntities<GroupCamera>())
+            {
+                
+
+            }
+        }
+    }
+}
