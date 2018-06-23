8e1934cd610d7501bbfbf00404389ea8a1b2333e
Wenish <jonas.voland@bluewin.ch>
2018-06-23 02:53:36 +0200

    update

---
+++ blubSense/Assets/Scripts/Systems/SystemCameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Assets.Scripts.Systems
+{
+    public class SystemCameraMovement : ComponentSystem
+    {
+        private struct Group
+        {
+            public Camera Camera;
+        }
+
+        protected override void OnUpdate()
+        {
+            foreach (var entity in GetEntities<Group>())
+            {
+
+
+            }
+        }
+    }
+}
---
+++ blubSense/Assets/Scripts/Systems/SystemCameraTarget.cs
@@ -0,0 +1,37 @@
+﻿using Assets.Scripts.Components;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Assets.Scripts.Systems
+{
+    public class SystemCameraTarget : ComponentSystem
+    {
+        private struct GroupCamera
+        {
+            public Camera Camera;
+            public Target Target;
+        }
+
+        private struct GroupPlayer
+        {
+            public Transform Transform;
+            public Player Player;
+        }
+
+        protected override void OnUpdate()
+        {
+            foreach (var camera in GetEntities<GroupCamera>())
+            {
+                foreach(var player in GetEntities<GroupPlayer>())
+                {
+                    if (player.Player.IsCurrentPlayer)
+                    {
+                        camera.Target.Value = player.Transform;
+                    }
+                }
+            }
+        }
+    }
+}
